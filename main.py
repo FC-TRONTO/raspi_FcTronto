e0b75e23c29430b067d07a8c166516c7fe7c1bfb
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-02-09 19:38:29 +0900

    Fix: デバッグ用出力部分を修正

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from ctypes import Structure, c_int, c_double
 import os
 from serialCom import SerialController
 from motorContl import MotorController
+from debug import ERROR, WARN, INFO, DEBUG, TRACE
 
 # 共有メモリの構造体
 class Point(Structure):
     _fields_ = [('irAngle', c_int), ('uSonicDis', c_double), ('enemyGoalAngle', c_int), ('enemyGoalDis', c_int), ('myGoalAngle', c_int), ('myGoalDis', c_int)]
 
 def info(title):
-    print(title)
-    print('module name:', __name__)
-    print('parent process:', os.getppid())
-    print('process id:', os.getpid())
+    INFO(title)
+    INFO('module name:', __name__)
+    INFO('parent process:', os.getppid())
+    INFO('process id:', os.getpid())
 
 if __name__ == '__main__':
     info('main line')
@@ -27,9 +28,9 @@ if __name__ == '__main__':
     p_serialCon = Process(target=serialController.target, args=(shmem,))
     p_motorContl = Process(target=motorController.target, args=(shmem, serialController))
     p_serialCon.start()
-    print('p_serialCon started')
+    DEBUG('p_serialCon started')
     p_motorContl.start()
-    print('p_motorContl started')
-    p_motorContl.join()
+    DEBUG('p_motorContl started')
     p_serialCon.join()
+    p_motorContl.join()
 
