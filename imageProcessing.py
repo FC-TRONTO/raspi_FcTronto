5585c66cb53203321905143d75a9548c2c3a3a2f
kenbantataki <r.yamamoto2357@gmail.com>
2019-02-09 11:56:21 +0900

    Add: 最大の青色領域をマークする

--- imageProcessing.py
+++ imageProcessing.py
@@ -11,16 +11,27 @@ class ImageProcessing:
     def __init__(self):
         print 'ImageProcessing generated'
 
-    def red_detect(self, img):
-        # HSV色空間に変換
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    def yellow_detect(self, hsv_img):
+        # 黄色のHSVの値域
+        hsv_min = np.array([20, 127, 0])
+        hsv_max = np.array([40, 255, 255])
+        mask = cv2.inRange(hsv_img, hsv_min, hsv_max)
 
-        # 赤色のHSVの値域1
+        return mask
+
+    def blue_detect(self, hsv_img):
+        # 青色のHSVの値域
         hsv_min = np.array([100, 127, 30])
         hsv_max = np.array([120, 255, 255])
-        mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
+        mask = cv2.inRange(hsv_img, hsv_min, hsv_max)
 
-        return mask1
+        return mask
+
+    def draw_marker(self, img, x, y, marker_color):
+        cv2.line(img, (x - 7, y), (x + 7, y), color=(255, 255, 255), thickness=2)
+        cv2.line(img, (x, y - 7), (x, y + 7), color=(255, 255, 255), thickness=2)
+        cv2.line(img, (x - 7, y), (x + 7, y), color=marker_color, thickness=1)
+        cv2.line(img, (x, y - 7), (x, y + 7), color=marker_color, thickness=1)
 
     def imageProcessingMain(self, shmem):
         # 画像処理を行う
@@ -28,53 +39,48 @@ class ImageProcessing:
         with picamera.PiCamera() as camera:
             with picamera.array.PiRGBArray(camera) as stream:
                 camera.resolution = (640, 480)
-                while True:
+                cap = cv2.VideoCapture('test.avi')
+
+                while cap.isOpened():
                     print 'imageProcessingMain'
                     # TODO: 画像を取得する
                     # stream.arrayにRGBの順で映像データを格納
                     camera.capture(stream, 'bgr', use_video_port=True)
+                    #stream = cv2.imread('sample_match.jpg')
 
-                    # 赤色検出
-                    mask = self.red_detect(stream.array)
+                    # HSV色空間に変換
+                    hsv_img = cv2.cvtColor(stream.array, cv2.COLOR_BGR2HSV)
 
-                    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    # 青色検出
+                    mask_blue = self.blue_detect(hsv_img)
+                    mask_yellow = self.yellow_detect(hsv_img)
+                    cv2.imshow("Mask blue", mask_blue)
+                    cv2.imshow("Mask yellow", mask_yellow)
+                    contours_blue, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    contours_yellow, _ = cv2.findContours(mask_yellow, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-                    rects = []
-                    for contour in contours:
+                    convex_hull_blue_list = []
+                    for contour in contours_blue:
                         approx = cv2.convexHull(contour)
                         rect = cv2.boundingRect(approx)
-                        rects.append(rect)
+                        cv2.rectangle(stream.array, (rect[0], rect[1]),  (rect[0] + rect[2], rect[1] + rect[3]), (255, 150, 100), thickness=1)
+                        # cv2.drawContours(stream.array, contour, -1, (255, 0, 0), 2)
 
-                    for rect in rects:
-                        print rect[0]
-                        print rect[1]
-                        print rect[2]
-                        print rect[3]
-                        # print rect[4]
+                        M = cv2.moments(approx)
+                        convex_hull_blue_list.append(M)
+                        print 'ConvexHull Area = ' + str(M['m00'])
+
+                    if len(convex_hull_blue_list) > 0:
+                        max_convex_hull_blue = max(convex_hull_blue_list, key=(lambda x: x['m00']))
+
+                        if max_convex_hull_blue['m00'] > 0 :
+                            cx = int(max_convex_hull_blue['m10'] / max_convex_hull_blue['m00'])
+                            cy = int(max_convex_hull_blue['m01'] / max_convex_hull_blue['m00'])
+                            self.draw_marker(stream.array, cx, cy, (255, 30, 30))
 
 
-                        cv2.rectangle(stream.array, (rect[0], rect[1]),  (rect[0] + rect[2], rect[1] + rect[3]), (0, 0, 255), thickness=2)
-
-                        # 結果表示
+                    # 結果表示
                     cv2.imshow("Frame", stream.array)
-                    cv2.imshow("Mask", mask)
-
-
-                    # # グレースケールに変換
-                    # gray = cv2.cvtColor(stream.array, cv2.COLOR_BGR2GRAY)
-                    #
-                    # # カスケードファイルを利用して顔の位置を見つける
-                    # cascade = cv2.CascadeClassifier(cascade_file)
-                    # face_list = cascade.detectMultiScale(gray, minSize=(100, 100))
-                    #
-                    # for (x, y, w, h) in face_list:
-                    #     print("face_position:", x, y, w, h)
-                    #     color = (0, 255, 255)
-                    #     pen_w = 1
-                    #     cv2.rectangle(stream.array, (x, y), (x + w, y + h), color, thickness=pen_w)
-                    # # system.arrayをウィンドウに表示
-                    # cv2.imshow('frame', stream.array)
-                    # # cv2.imshow('frame2', gray)#
 
                     # "q"でウィンドウを閉じる
                     if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -89,33 +95,6 @@ class ImageProcessing:
             # TODO: ゴールの位置を解析する
 
             # TODO: 共有メモリに書き込む
-
-
-
-
-
-
-            # cap = cv2.VideoCapture("input.mp4")
-            #
-            # while (cap.isOpened()):
-            #     # フレームを取得
-            #     ret, frame = cap.read()
-            #
-            #     # 赤色検出
-            #     mask = red_detect(frame)
-            #
-            #     # 結果表示
-            #     cv2.imshow("Frame", frame)
-            #     cv2.imshow("Mask", mask)
-            #
-            #     # qキーが押されたら途中終了
-            #     if cv2.waitKey(25) & 0xFF == ord('q'):
-            #         break
-            #
-            #     time.sleep(1)
-            #
-            # cap.release()
-            # cv2.destroyAllWindows()
 
     def target(self, shmem):
         print 'imageProcessingMain target() start'
