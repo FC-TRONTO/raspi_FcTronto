63c955316e80d00b5635403951ba9fd907866aaa
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-02-10 19:38:41 +0900

    Fix: main.pyの起動・終了処理を修正

--- webiopi/static/script.py
+++ webiopi/static/script.py
@@ -1,16 +1,25 @@
 import webiopi
 import sys
 import os
+import subprocess
+from subprocess import Popen
 
 @webiopi.macro
 def start(data):
     # os.systemを使ってmain.pyを実行する
-    os.system('python /home/pi/Desktop/raspi_FcTronto/main.py')
+    #os.system('python /home/pi/Desktop/raspi_FcTronto/main.py')
+    cmd = "python /home/pi/Desktop/raspi_FcTronto/main.py"
+    #subprocess.call(cmd.strip().split(" "))
+    output_file = '/home/pi/Desktop/raspi_FcTronto/debug.log'
+    with open(output_file, 'w') as f:
+        Popen(cmd.strip().split(" "), stdout=f)
 
 @webiopi.macro
 def stop(data):
     # os.systemを使ってstop_main_py.shを実行する
-    os.system('./stop_main_py.sh')
+    #os.system('ps auxww | grep main.py | grep -v grep | awk '{ print "sudo kill -9", $2}' | sh')"
+    #subprocess.call("/home/pi/Desktop/raspi_FcTronto/webiopi/static/stop_main_py.sh")
+    Popen("/home/pi/Desktop/raspi_FcTronto/webiopi/static/stop_main_py.sh")
 
 @webiopi.macro
 def set_goal_mode(color):
