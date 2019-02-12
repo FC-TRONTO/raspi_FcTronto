728f4fe807114fcf3287a172cd42b8e715c704c0
kenbantataki <r.yamamoto2357@gmail.com>
2019-02-13 00:00:17 +0900

    Update: パラメータ調整、壁の検知

--- imageProcessing.py
+++ imageProcessing.py
@@ -20,13 +20,21 @@ class EnemyGoalColorE(Enum):
 class ImageProcessing:
     ENEMY_GOAL_COLOR = EnemyGoalColorE.YELLOW
 
-    BLUE_HSV_RANGE_MIN = [100, 127, 30]
-    BLUE_HSV_RANGE_MAX = [120, 255, 255]
-    YELLOW_HSV_RANGE_MIN = [20, 127, 0]
-    YELLOW_HSV_RANGE_MAX = [40, 255, 255]
+    BLUE_HSV_RANGE_MIN = [55, 70, 10]
+    BLUE_HSV_RANGE_MAX = [120, 150, 40]
+    YELLOW_HSV_RANGE_MIN = [15, 127, 30]
+    YELLOW_HSV_RANGE_MAX = [30, 255, 255]
+    WALL_HSV_RANGE_MIN = [25, 70, 0]
+    WALL_HSV_RANGE_MAX = [120, 255, 255]
 
-    CAMERA_CENTER_CX = 240
-    CAMERA_CENTER_CY = 240
+    CAMERA_CENTER_CX_T = 220
+    CAMERA_CENTER_CY_T = 240
+    CAMERA_CENTER_CX = CAMERA_CENTER_CY_T
+    CAMERA_CENTER_CY = CAMERA_CENTER_CX_T
+    CAMERA_RANGE_R = 170
+
+    KARNEL_R = 5
+    KARNEL_SIZE = 2 * KARNEL_R + 1
 
     GOAL_DISTANCE_TABLE = []
 
@@ -38,8 +46,6 @@ class ImageProcessing:
         # テーブルの読み込み
         # TODO: テーブル情報は仮。csvファイルから読み込むようにする
         self.GOAL_DISTANCE_TABLE = range(241)
-        TRACE(self.GOAL_DISTANCE_TABLE)
-        TRACE(self.GOAL_DISTANCE_TABLE[234])
 
     # @brief 敵ゴールの色をsetする
     # @param set_color setする相手ゴールの色
@@ -57,7 +63,12 @@ class ImageProcessing:
     # @param color_name 検知する色の名前
     # @detail
     def colorDetect(self, hsv_img, hsv_range_min, hsv_range_max, color_name):
+        mask_0 = np.zeros((480, 480, 1), np.uint8)
+        cv2.circle(mask_0, (self.CAMERA_CENTER_CX_T, self.CAMERA_CENTER_CY_T), self.CAMERA_RANGE_R, 255, thickness=-1)
+
         mask = cv2.inRange(hsv_img, np.array(hsv_range_min), np.array(hsv_range_max))
+        mask = cv2.bitwise_and(mask, mask, mask=mask_0)
+
         # 画角の前後左右と画像表示の上下左右を揃えるために画像を転置する。
         cv2.imshow('Mask' + color_name, mask.transpose((1, 0)))
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -70,21 +81,62 @@ class ImageProcessing:
             #               thickness=1)
 
             M = cv2.moments(approx)
-            convex_hull_list.append(M)
+            convex_hull_list.append({'approx': approx, 'moment': M})
 
         if len(convex_hull_list) > 0:
-            max_convex_hull_blue = max(convex_hull_list, key=(lambda x: x['m00']))
+            max_convex_hull = max(convex_hull_list, key=(lambda x: x['moment']['m00']))
+            if max_convex_hull['moment']['m00'] > 0:
+                area_size = max_convex_hull['moment']['m00']
 
-            if max_convex_hull_blue['m00'] > 0:
-                area_size = max_convex_hull_blue['m00']
+                cx = int(max_convex_hull['moment']['m10'] / max_convex_hull['moment']['m00'])
+                cy = int(max_convex_hull['moment']['m01'] / max_convex_hull['moment']['m00'])
 
-                cx = int(max_convex_hull_blue['m10'] / max_convex_hull_blue['m00'])
-                cy = int(max_convex_hull_blue['m01'] / max_convex_hull_blue['m00'])
-
-                return cx, cy, area_size
-            return -1, -1, 0.0
+                return cx, cy, area_size, max_convex_hull['approx']
+            return -1, -1, 0.0, []
         else:
-            return -1, -1, 0.0
+            return -1, -1, 0.0, []
+
+    # @brief フィールド領域を検知する
+    # @param hsv_img HSV変換後の処理対象画像
+    # @param hsv_range_min 検知する色範囲の最小値
+    # @param hsv_range_max 検知する色範囲の最大値
+    # @param color_name 検知する色の名前
+    # @detail
+    def wallDetect(self, hsv_img, hsv_range_min, hsv_range_max, color_name):
+        mask_0 = np.zeros((480, 480, 1), np.uint8)
+        cv2.circle(mask_0, (self.CAMERA_CENTER_CX_T, self.CAMERA_CENTER_CY_T), self.CAMERA_RANGE_R, 255,
+                   thickness=-1)
+
+        mask = cv2.inRange(hsv_img, np.array(hsv_range_min), np.array(hsv_range_max))
+        mask = cv2.bitwise_and(mask, mask, mask=mask_0)
+        # TODO: initに移動する
+        kernel = np.zeros((self.KARNEL_SIZE, self.KARNEL_SIZE), np.uint8)
+        cv2.circle(kernel, (self.KARNEL_R, self.KARNEL_R), self.KARNEL_R, 1, thickness=-1)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+        TRACE(kernel)
+        # 画角の前後左右と画像表示の上下左右を揃えるために画像を転置する。
+        cv2.imshow('Mask' + color_name, mask.transpose((1, 0)))
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        convex_hull_list = []
+        for contour in contours:
+            approx = cv2.convexHull(contour)
+
+            M = cv2.moments(approx)
+            convex_hull_list.append({'approx': approx, 'moment': M})
+
+        if len(convex_hull_list) > 0:
+            max_convex_hull = max(convex_hull_list, key=(lambda x: x['moment']['m00']))
+            if max_convex_hull['moment']['m00'] > 0:
+                area_size = max_convex_hull['moment']['m00']
+
+                cx = int(max_convex_hull['moment']['m10'] / max_convex_hull['moment']['m00'])
+                cy = int(max_convex_hull['moment']['m01'] / max_convex_hull['moment']['m00'])
+
+                return cx, cy, area_size, max_convex_hull['approx']
+            return -1, -1, 0.0, []
+        else:
+            return -1, -1, 0.0, []
 
     # @brief 十字マーカーを描画する
     # @param x 十字マーカーのx座標
@@ -114,6 +166,61 @@ class ImageProcessing:
 
         return int(goal_angle), int(goal_distance)
 
+    def imageProcessingFrame(self, shmem, stream):
+        # HSV色空間に変換
+        hsv_img = cv2.cvtColor(stream.array, cv2.COLOR_BGR2HSV)
+
+        # 黄色領域の検知
+        yellow_cx_t, yellow_cy_t, yellow_area_size, yellow_convex = self.colorDetect(hsv_img, self.YELLOW_HSV_RANGE_MIN,
+                                                                                     self.YELLOW_HSV_RANGE_MAX,
+                                                                                     'Yellow')
+        if yellow_cx_t > -1:
+            self.draw_marker(stream.array, yellow_cx_t, yellow_cy_t, (30, 255, 30))
+
+        # 青色領域の検知
+        blue_cx_t, blue_cy_t, blue_area_size, blue_convex = self.colorDetect(hsv_img, self.BLUE_HSV_RANGE_MIN,
+                                                                             self.BLUE_HSV_RANGE_MAX,
+                                                                             'Blue')
+        if blue_cx_t > -1:
+            self.draw_marker(stream.array, blue_cx_t, blue_cy_t, (255, 30, 30))
+
+        DEBUG('Yellow :' + str(yellow_area_size).rjust(8), 'Blue :' + str(blue_area_size).rjust(8))
+
+        # 壁の検知
+        self.colorDetect(hsv_img, self.WALL_HSV_RANGE_MIN, self.WALL_HSV_RANGE_MAX, 'Wall')
+
+        # 画角の前後左右と画像表示の上下左右を揃えるためにx座標とy座標を交換する。
+        yellow_cx = yellow_cy_t
+        yellow_cy = yellow_cx_t
+        blue_cx = blue_cy_t
+        blue_cy = blue_cx_t
+
+        yellow_goal_angle, yellow_goal_distance = self.calcGoalDirection(yellow_cx, yellow_cy, yellow_area_size)
+        blue_goal_angle, blue_goal_distance = self.calcGoalDirection(blue_cx, blue_cy, blue_area_size)
+        DEBUG('Yellow : Angle=' + str(yellow_goal_angle).rjust(4), 'Dis=' + str(yellow_goal_distance).rjust(3),
+              'Blue : Angle=' + str(blue_goal_angle).rjust(4), 'Dis=' + str(blue_goal_distance).rjust(3))
+
+        # 結果表示
+        # 画角の前後左右と画像表示の上下左右を揃えるために画像を転置する。
+        cv2.imshow('Frame', stream.array.transpose((1, 0, 2)))
+        cv2.moveWindow('Frame', 0, 30)
+        cv2.moveWindow('MaskYellow', 482, 30)
+        cv2.moveWindow('MaskBlue', 964, 30)
+
+        # 共有メモリに書き込む
+        if self.ENEMY_GOAL_COLOR == EnemyGoalColorE.YELLOW:
+            shmem.enemyGoalAngle = int(yellow_goal_angle)
+            shmem.enemyGoalDis = int(yellow_goal_distance)
+            shmem.myGoalAngle = int(blue_goal_angle)
+            shmem.myGoalDis = int(blue_goal_distance)
+        elif self.ENEMY_GOAL_COLOR == EnemyGoalColorE.BLUE:
+            shmem.enemyGoalAngle = int(blue_goal_angle)
+            shmem.enemyGoalDis = int(blue_goal_distance)
+            shmem.myGoalAngle = int(yellow_goal_angle)
+            shmem.myGoalDis = int(yellow_goal_distance)
+        else:
+            ERROR('Invalid Error : ENEMY_GOAL_COLOR =', self.ENEMY_GOAL_COLOR)
+
     # @brief 画像処理のmain処理
     # @param shmem 共有メモリ
     def imageProcessingMain(self, shmem):
@@ -127,56 +234,8 @@ class ImageProcessing:
                 while cap.isOpened():
                     # 画像を取得し、stream.arrayにRGBの順で映像データを格納
                     camera.capture(stream, 'bgr', use_video_port=True)
-                    # stream = cv2.imread('sample_match.jpg')
 
-                    # HSV色空間に変換
-                    hsv_img = cv2.cvtColor(stream.array, cv2.COLOR_BGR2HSV)
-
-                    # 黄色領域の検知
-                    yellow_cx_t, yellow_cy_t, yellow_area_size = self.colorDetect(hsv_img, self.YELLOW_HSV_RANGE_MIN,
-                                                                self.YELLOW_HSV_RANGE_MAX, 'Yellow')
-                    if yellow_cx_t > -1:
-                        self.draw_marker(stream.array, yellow_cx_t, yellow_cy_t, (30, 255, 30))
-                        
-                    # 青色領域の検知
-                    blue_cx_t, blue_cy_t, blue_area_size = self.colorDetect(hsv_img, self.BLUE_HSV_RANGE_MIN, self.BLUE_HSV_RANGE_MAX,
-                                                              'Blue')
-                    if blue_cx_t > -1:
-                        self.draw_marker(stream.array, blue_cx_t, blue_cy_t, (255, 30, 30))
-
-                    DEBUG('Yellow :' + str(yellow_area_size).rjust(8), 'Blue :' + str(blue_area_size).rjust(8))
-
-                    # 画角の前後左右と画像表示の上下左右を揃えるためにx座標とy座標を交換する。
-                    yellow_cx = yellow_cy_t
-                    yellow_cy = yellow_cx_t
-                    blue_cx = blue_cy_t
-                    blue_cy = blue_cx_t
-
-                    yellow_goal_angle, yellow_goal_distance = self.calcGoalDirection(yellow_cx, yellow_cy, yellow_area_size)
-                    blue_goal_angle, blue_goal_distance = self.calcGoalDirection(blue_cx, blue_cy, blue_area_size)
-                    DEBUG('Yellow : Angle=' + str(yellow_goal_angle).rjust(4), 'Dis=' + str(yellow_goal_distance).rjust(3),
-                          'Blue : Angle=' + str(blue_goal_angle).rjust(4), 'Dis=' + str(blue_goal_distance).rjust(3))
-
-                    # 結果表示
-                    # 画角の前後左右と画像表示の上下左右を揃えるために画像を転置する。
-                    cv2.imshow('Frame', stream.array.transpose((1, 0, 2)))
-                    cv2.moveWindow('Frame', 0, 30)
-                    cv2.moveWindow('MaskYellow', 482, 30)
-                    cv2.moveWindow('MaskBlue', 964, 30)
-
-                    # 共有メモリに書き込む
-                    if self.ENEMY_GOAL_COLOR == EnemyGoalColorE.YELLOW:
-                        shmem.enemyGoalAngle = int(yellow_goal_angle)
-                        shmem.enemyGoalDis = int(yellow_goal_distance)
-                        shmem.myGoalAngle = int(blue_goal_angle)
-                        shmem.myGoalDis = int(blue_goal_distance)
-                    elif self.ENEMY_GOAL_COLOR == EnemyGoalColorE.BLUE:
-                        shmem.enemyGoalAngle = int(blue_goal_angle)
-                        shmem.enemyGoalDis = int(blue_goal_distance)
-                        shmem.myGoalAngle = int(yellow_goal_angle)
-                        shmem.myGoalDis = int(yellow_goal_distance)
-                    else:
-                        ERROR('Invalid Error : ENEMY_GOAL_COLOR =', self.ENEMY_GOAL_COLOR)
+                    self.imageProcessingFrame(shmem, stream)
 
                     # "q"でウィンドウを閉じる
                     if cv2.waitKey(1) & 0xFF == ord("q"):
