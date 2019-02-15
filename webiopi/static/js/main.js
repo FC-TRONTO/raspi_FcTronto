9c981ca2d86c56ef14f5488cff3b22bc374b7ef9
kouya17 <17383696+kouya17@users.noreply.github.com>
2019-02-15 09:51:55 +0900

    Update: 設定関数実装

--- webiopi/static/js/main.js
+++ webiopi/static/js/main.js
@@ -19,6 +19,38 @@ function set_motor_setting(setting) {
 function set_goal_mode(color) {
 	webiopi().callMacro("set_goal_mode", color, callbackGetValue);
 }
+
+function set_shoot_algo(num) {
+	webiopi().callMacro("set_shoot_algo", num, callbackGetValue);
+}
+
+function set_chase_algo(num) {
+	webiopi().callMacro("set_chase_algo", num, callbackGetValue);
+}
+
+function set_shoot_speed(speed) {
+	webiopi().callMacro("set_shoot_speed", speed, callbackGetValue);
+}
+
+function set_k_shoot_angle(k) {
+	webiopi().callMacro("set_k_shoot_angle", k, callbackGetValue);
+}
+
+function set_chase_speed(speed) {
+	webiopi().callMacro("set_chase_speed", speed, callbackGetValue);
+}
+
+function set_k_chase_angle(k) {
+	webiopi().callMacro("set_k_chase_angle", k, callbackGetValue);
+}
+
+function set_go_center(speed) {
+	webiopi().callMacro("set_go_center", speed, callbackGetValue);
+}
+
+function set_k_go_center(k) {
+	webiopi().callMacro("set_k_go_center", k, callbackGetValue);
+}
  
 function callbackGetValue(macro, args, data) {
 	console.log(macro);
--- webiopi/static/script.py
+++ webiopi/static/script.py
@@ -39,3 +39,42 @@ def set_goal_mode(color):
         else:
             f.write('none')
 
+@webiopi.macro
+def set_shoot_algo(num):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/shoot_algo.txt', mode='w') as f:
+        f.write(num)
+
+@webiopi.macro
+def set_chase_algo(num):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/chase_algo.txt', mode='w') as f:
+        f.write(num)
+
+@webiopi.macro
+def set_shoot_speed(speed):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/shoot_speed.txt', mode='w') as f:
+        f.write(speed)
+
+@webiopi.macro
+def set_k_shoot_angle(k):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/k_shoot_angle.txt', mode='w') as f:
+        f.write(k)
+
+@webiopi.macro
+def set_chase_speed(speed):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/chase_speed.txt', mode='w') as f:
+        f.write(speed)
+
+@webiopi.macro
+def set_k_chase_angle(k):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/k_chase_angle.txt', mode='w') as f:
+        f.write(k)
+
+@webiopi.macro
+def set_go_center(speed):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/center_speed.txt', mode='w') as f:
+        f.write(speed)
+
+@webiopi.macro
+def set_k_go_center(k):
+    with open('/home/pi/Desktop/raspi_FcTronto/webiopi/k_center_angle.txt', mode='w') as f:
+        f.write(k)
